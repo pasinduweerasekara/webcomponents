7ff6700e31fa5ebd1af598246bb55b43a850f1bf
pasinduweerasekara <wmpmweerasekara@gmail.com>
2023-08-25 01:33:31 +0530

    navbar variables add

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,53 +1,81 @@
-import React,{useState} from 'react'
-import './navbar.css'
-import logo from '../../statics/logo.svg' //import logo
-import searchIcon from '../../statics/search-thin.svg' //import search icon
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import "./navbar.css";
+import logo from "../../statics/logo.svg"; //import logo
+import searchIcon from "../../statics/search-thin.svg"; //import search icon
+import { Link } from "react-router-dom";
 
-
-
-
+// search-thin.svg is required
 
 function NavBar() {
-    const [ismobile, setismobile] = useState(false);
-  
-  function handleMenuBtnClick() {
-    setismobile(!ismobile)
+  const [ismobile, setismobile] = useState(false);
 
+  function handleMenuBtnClick() {
+    setismobile(!ismobile);
   }
 
   return (
-    <nav className='nav-container'>
+    <nav className="nav-container">
       <div className="logo">
         <img src={logo} alt="logo.svg" id="logo-img" />
       </div>
-      <ul className={ismobile?"link-list menu-open":"link-list" }>
-        <Link to='/' className="link-item active" >
-            <li className="link-text" onClick={()=>{setismobile(false)} }>home</li>
+      <ul className={ismobile ? "link-list menu-open" : "link-list"}>
+        <Link to="/" className="link-item active">
+          <li
+            className="link-text"
+            onClick={() => {
+              setismobile(false);
+            }}
+          >
+            home
+          </li>
         </Link>
 
-        <Link to='/downloads' className="link-item" >
-            <li className="link-text" onClick={()=>{setismobile(false)} }>downloads</li>
+        <Link to="/downloads" className="link-item">
+          <li
+            className="link-text"
+            onClick={() => {
+              setismobile(false);
+            }}
+          >
+            downloads
+          </li>
         </Link>
 
-        <Link to='/about' className="link-item">
-            <li className="link-text" onClick={()=>{setismobile(false)} }>about</li>
+        <Link to="/about" className="link-item">
+          <li
+            className="link-text"
+            onClick={() => {
+              setismobile(false);
+            }}
+          >
+            about
+          </li>
         </Link>
 
-        <Link to='/contacts' className="link-item">
-            <li className="link-text" onClick={()=>{setismobile(false)} }>contacts</li>
+        <Link to="/contacts" className="link-item">
+          <li
+            className="link-text"
+            onClick={() => {
+              setismobile(false);
+            }}
+          >
+            contacts
+          </li>
         </Link>
         <div className="search-bar">
-          <img src={searchIcon} alt={searchIcon} id="search-icon"/>
-      </div>
+          <img src={searchIcon} alt={searchIcon} id="search-icon" />
+        </div>
       </ul>
-      <div className={ismobile?"menu-btn close":"menu-btn"} onClick={handleMenuBtnClick}>
+      <div
+        className={ismobile ? "menu-btn close" : "menu-btn"}
+        onClick={handleMenuBtnClick}
+      >
         <span className="menu-bar" id="bar1"></span>
         <span className="menu-bar" id="bar2"></span>
         <span className="menu-bar" id="bar3"></span>
-    </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
